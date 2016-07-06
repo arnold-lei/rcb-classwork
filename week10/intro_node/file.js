bac58b0616d73b254740aee0b173bc7dc60f95e4
Arnold Lei <arnold.lei88@gmail.com>
2016-07-06 13:15:31 -0400

    some experiments with node npm

---
+++ week10/intro_node/file.js
@@ -0,0 +1,7 @@
+exports.user = {
+	firstName: 'Arnold',
+	lastName: 'Lei',
+	address: '430 Magnolia St, Highland Park NJ 08904',
+	phoneNumber: '(609) 647-2091',
+	id:1
+}
--- week10/intro_node/hello.js
+++ week10/intro_node/hello.js
@@ -1,1 +1,11 @@
-console.log('Hello Word!');
+var file = require('./file.js');
+
+var geocoder = require('geocoder');
+
+geocoder.geocode(process.argv[2], function(err, data){
+	if(!err){
+		console.log(JSON.stringify(data));
+	}
+})
+
+
