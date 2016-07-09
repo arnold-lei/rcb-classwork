a9a534dd6ce1c5c97c16c850061065682c2a5e99
Arnold Lei <arnold.lei88@gmail.com>
2016-07-09 10:36:22 -0400

    Added sat's class excercise

---
+++ week10/intro_node/programmer.js
@@ -0,0 +1,44 @@
+// Instructions
+
+// In this activity, we are going to create a constructor function called "Programmer" which can be used to create objects containing information on various programmers.
+
+// Your "Programmer" constructor should be able to take in the following information...
+
+// The programmer's name
+// Their position/job title
+// How old they are
+// Favorite programming language
+// Now create a method for the constructor that would print all of the information contained within an object to the console.
+
+// Finally, create a "Programmer" object and call the method to print its contents
+
+var inquirer = require('inquirer');
+
+var Programmer = function(id){
+	this.name = id.name,
+	this.jobTitle = id.jobTitle,
+	this.age = id.age,
+	this.language = id.language,
+	this.printInfo = function(){
+		console.log('Hi my name is ' + this.name + ', I am ' + this.age + 'years old' + ' i am currently working as a '+ this.position + 'and I like to program in ' + this.language);
+	}
+}
+
+inquirer.prompt({
+	{
+		name: 'name',
+		message: ' What is your name?',
+	},
+	{
+		name: 'position',
+		message: 'What is your current position'
+	},
+	{
+		name: 'age',
+		message: 'How old are you?'
+	},
+	{
+		name: 'language',
+		message: 'What is your favourite langauge'
+	}
+})
